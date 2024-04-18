8b7a6f1f223d8d91b11d921ba30c7145e82d8d7a
Mayank Bharti <22cs3039@gmail.com>
2024-04-18 20:40:50 +0530

    changes

--- Backend_webd/src/models/product.js
+++ Backend_webd/src/models/product.js
@@ -6,13 +6,9 @@ const { required } = require('nodemon/lib/config');
 const productSchema = new mongoose.Schema({
     "Product Type": { type: String, required: true },
     Brand: { type: String, required: true },
-    Price: { type: Number, required: true },
+    Price: { type: String, required: true },
+    Rating: { type: String,required:true },
     Image: { type: String, required: true },
-    img1: { type: String },
-    img2: { type: String },
-    img3: { type: String },
-    img4: { type: String },
-    img5: { type: String }
  });
 const Product = mongoose.model('product', productSchema);
 
